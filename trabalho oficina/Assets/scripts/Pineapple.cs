49d2ec4e48dfb4c30fb841fd62fc09d2b475d75c
Ruama017 <145566985+Ruama017@users.noreply.github.com>
2024-08-30 23:56:16 -0300

    aula 8

--- trabalho oficina/Assets/scripts/Pineapple.cs
+++ trabalho oficina/Assets/scripts/Pineapple.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Pineapple : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+
+    public GameObject collected;
+    public int Score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
@@ -18,7 +25,13 @@ public class Pineapple : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.gameObject.tag =="Player"){
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
 
-        }
+            GameController.instance.totalScore += Score;
+
+            Destroy(gameObject, 0.25f);
+        } 
     }
 }
