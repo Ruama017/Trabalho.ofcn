ea452bd220487e7ba574d5d05742c2af3c65c4b1
Ruama017 <145566985+Ruama017@users.noreply.github.com>
2024-09-16 20:48:33 -0300

    video 3

--- trabalho oficina/Assets/scripts/EfeitoDigital.cs
+++ trabalho oficina/Assets/scripts/EfeitoDigital.cs
@@ -11,30 +11,57 @@ public class EfeitoDigital : MonoBehaviour
     private AudioSource _audioSource;
     private string mensagemOriginal;
     public bool imprimindo;
+    public float tempoEntreLetras = 0.08f;
 
 
     private void Awake()
     {
+        TryGetComponent(out componenteTexto);
+        TryGetComponent(out _audioSource);
+        mensagemOriginal = componenteTexto.text;
+        componenteTexto.text = "";
+    
 
     }
 
     private void OnEnable()
     {
+        ImprimirMensagem(mensagemOriginal);
 
     }
 
 
     private void OnDisable()
     {
+        componenteTexto.text = mensagemOriginal;
+        StopAllCoroutines();
 
     }
 
-    public void ImprimirMensagem(string msg)
+    public void ImprimirMensagem(string mensagem)
     {
+        if(gameObject.activeInHierarchy)
+        {
+            if(imprimindo) return;
+            imprimindo = true;
+            StartCoroutine(LetraPorLetra(mensagem));
+        }
 
     }
 
-    IEnumerator LetraPorLetra(string msg){}
+    IEnumerator LetraPorLetra(string mensagem){
+        string msg = "";
+        foreach(var letra in mensagem){
+            msg+= letra;
+            componenteTexto.text = msg;
+            _audioSource.Play();
+            yield return new WaitForSeconds(tempoEntreLetras);
+        }
+
+        imprimindo = false;
+        StopAllCoroutines();
+
+    }
     
 
     
